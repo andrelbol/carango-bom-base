464f680e6e7902bca847a39e390f8ae93f05fed4
Andre Borges <andre.borges@thoughtworks.com>
2021-07-08 11:23:34 -0300

    Adicionada busca no backend dos dados do dashboard

--- src/models/DadosDashboard.js
+++
@@ -1,9 +0,0 @@
-class DadosDashboard {
-  constructor(nomeMarca, quantidadeVeiculos, valor) {
-    this.nomeMarca = nomeMarca;
-    this.quantidadeVeiculos = quantidadeVeiculos;
-    this.valor = valor;
-  }
-}
-
-export default DadosDashboard;
--- src/services/DashboardService.js
+++ src/services/DashboardService.js
@@ -1,18 +1,11 @@
 import BaseService from './BaseService';
-import DadosDashboard from '../models/DadosDashboard';
+import settings from '../config/settings.dev';
 
 
 class DashboardService extends BaseService {
   
   getDadosDashboard() {
-    const mockedData = [
-      new DadosDashboard("Marca1", 10, 1000),
-      new DadosDashboard("Marca2", 20, 2000),
-      new DadosDashboard("Marca3", 30, 3000),
-      new DadosDashboard("Marca4", 40, 4000),
-    ]
-
-    return Promise.resolve(mockedData);
+    return this.request(`${settings.baseUrl}/marcas/dashboard`);
   }
 }
 
