ca14da5bd2c926afab5d663550957e2b9a0b811e
Faneli <marcos.faneli@thoughtworks.com>
2021-07-09 16:49:33 -0400

    Teste da tela de dashboard

---
+++ src/pages/Dashboard/Dashboard.test.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { act, render, screen } from "@testing-library/react";
+import { Router } from "react-router-dom";
+import Dashboard from "./Dashboard";
+import { createMemoryHistory } from "history";
+import DashboardService from "../../services/DashboardService";
+
+jest.mock("../../services/dashBoardService");
+
+function renderWithProvider() {
+    const history = createMemoryHistory();
+    const rerender = render(
+        <Router history={history}>
+            <Dashboard />
+        </Router>
+    );
+    return { history, ...rerender }
+}
+
+test("deve exibir os cards por marca e seu valores e quantidade de veículos", async () => {
+    const cards = Promise.resolve([
+        { nomeMarca: "Marca 1", quantidadeVeiculos: 10, valor: 200000 },
+        { nomeMarca: "Marca 2", quantidadeVeiculos: 5, valor: 20000 },
+        { nomeMarca: "Marca 3", quantidadeVeiculos: 8, valor: 160000 }
+    ]);
+
+    DashboardService.mockImplementation(() => {
+        return {
+            getDadosDashboard: () => cards
+        };
+    });
+
+    renderWithProvider();
+    await act(() => cards);
+
+    const linhas = screen.getAllByText("Marca", {exact: false});
+
+    expect(linhas).toHaveLength(3);
+});
